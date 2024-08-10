8d33261c1df50b6d8e28c49c3175c146e015fec4
Cinder <148284411+CinerOC@users.noreply.github.com>
2024-08-11 01:12:59 +0300

    basic styling for the Login page

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,43 +1,68 @@
 import React from 'react'
 import classes from './LoginPage.module.css'
 import { Link } from 'react-router-dom';
+import ActionButton from '../../components/UI/Buttons/ActionButton/ActionButton'
 
 const LoginPage = () => {
   return (
     <div className={classes.contentContainer}>
       <main>
         <div className={classes.contentWrapper}>
-
           <form>
             <div className={classes.companyTitle}>
               <Link to='/introduction'>
-                <span>
-                  BloomBasket
-                </span>
+                <span>BloomBasket</span>
               </Link>
             </div>
             <div className={classes.authContentWrapper}>
-              <div className={classes.signInTitle}>
-                <span><b>Sign in</b></span>
-                <span>Don’t have an account? <a href="#">Create new</a></span>
+              <div className={classes.formTitle}>
+                <h1>Sign in</h1>
+                <span>
+                  Don’t have an account? 
+                  <a href="#">Create new</a>
+                </span>
               </div>
-
-              <div className={classes.signInFromWrapper}>
+              <div className={classes.inputsWrapper}>
                 <div>
-                  <span>E-mail</span>
+                  <div>E-mail</div>
                   <input type="text" />
                 </div>
                 <div>
-                  <span>Password</span>
+                  <div>Password</div>
                   <input type="text" />
                 </div>
+              </div>
+              <div className={classes.additionalOptionsWrapper}>
+                <div>
+                  <input type="checkbox" name="" value="true" />
+                  <span>Remember me</span>
+                </div>
+                <div>
+                  <a href="#">Forgot password?</a>
+                </div>
+              </div>
+              <div>
+                <ActionButton children={"Sign-in"}/>
+              </div>
+              <div className={classes.orSeparator}>
+                <hr />
+                <span>OR</span>
+                <hr />
+              </div>
+              <div className={classes.otherLoginMethod}>
+                <ActionButton children={"Continue with Google"} />
+                <ActionButton children={"Continue with VK"} />
               </div>
             </div>
           </form>
-
           <div className={classes.contentRight}>
+            <div className={classes.previewImgWrapper}></div>
+            <div className={classes.previewTitleWrapper}>
+              <h1>Introducing new features</h1>
+              <span>Dive deeper into the products you love. 
+                Our detailed product pages now offer comprehensive information</span>
+            </div>
           </div>
-
         </div>
       </main>
     </div>
